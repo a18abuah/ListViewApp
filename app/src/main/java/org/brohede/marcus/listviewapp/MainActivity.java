1e7e34ef4fc6c480a8ce741a2aa04b1253afa210
a18abuah <a18abuah@student.his.se>
2019-04-09 17:03:10 +0200

    Gjorde en toast

--- app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
+++ app/src/main/java/org/brohede/marcus/listviewapp/MainActivity.java
@@ -2,8 +2,11 @@ package org.brohede.marcus.listviewapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,6 +32,16 @@ public class MainActivity extends AppCompatActivity {
 
         ListView myListView= (ListView) findViewById(R.id.my_listview);
         myListView.setAdapter(adapter);
+
+       myListView.setOnItemClickListener(new AdapterView.OnItemClickListener()  {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               Toast.makeText( getApplicationContext(), "height: "+mountainHeights[position], Toast.LENGTH_LONG).show();
+
+           }
+
+
+        });
 
 
 
